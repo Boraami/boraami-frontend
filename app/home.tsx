89d5bf8b2f3e157cc2b8582a858e26bf8f83add3
fabcodingzest <fabrizvi786@gmail.com>
2024-02-10 02:18:00 +0530

    tested theme variables

--- app/home.tsx
+++ app/home.tsx
@@ -1,39 +1,56 @@
-import { View } from "react-native";
 import Constants from "expo-constants";
 import Storybook from "../.storybook";
-import { SizableText } from "tamagui";
+import { View, SizableText, Text } from "tamagui";
 
 function App() {
   return (
-    <View>
-      <SizableText fontFamily="$PoppinsReg" size={"$3xl"}>
+    <View backgroundColor={"$bg-brand-strong"}>
+      <Text color={"$text-primary"} theme="light">
+        Test theme
+      </Text>
+      <SizableText color={"$text-inactive"} fontFamily="$PoppinsReg" size={"$3xl"}>
         Poppins Regular - g
       </SizableText>
-      <SizableText fontFamily="$PoppinsReg" fontStyle="italic" size={"$3xl"}>
+      <SizableText
+        color={"$bg-neutral-subtle"}
+        fontFamily="$PoppinsReg"
+        fontStyle="italic"
+        size={"$3xl"}
+      >
         Poppins Regular Italic - g
       </SizableText>
-      <SizableText fontFamily="$btn" size={"$xl"}>
+      <SizableText color={"$bg-success-strong"} fontFamily="$btn" size={"$xl"}>
         Poppins Medium - g
       </SizableText>
-      <SizableText fontFamily="$btn" fontStyle="italic" size={"$xl"}>
+      <SizableText color={"$bg-success-strong"} fontFamily="$btn" fontStyle="italic" size={"$xl"}>
         Poppins Medium Italic - g
       </SizableText>
-      <SizableText fontFamily="$heading" size={"$xl"}>
+      <SizableText color={"$border-neutral"} fontFamily="$heading" size={"$xl"}>
         Poppins Bold - g
       </SizableText>
-      <SizableText fontFamily="$heading" fontStyle="italic" size={"$xl"}>
+      <SizableText
+        color={"$bg-success-subtle"}
+        fontFamily="$heading"
+        fontStyle="italic"
+        size={"$xl"}
+      >
         Poppins Bold Italic - g
       </SizableText>
-      <SizableText fontFamily="$body" size={"$4xl"} fontWeight="$true">
+      <SizableText color={"$text-error"} fontFamily="$body" size={"$4xl"} fontWeight="$true">
         Open Sans Regular - g
       </SizableText>
-      <SizableText fontFamily="$body" fontStyle="italic" size={"$4xl"}>
+      <SizableText color={"$bg-warning-subtle"} fontFamily="$body" fontStyle="italic" size={"$4xl"}>
         Open Sans Regular Italic - g
       </SizableText>
-      <SizableText fontFamily="$OpenSansBold" size={"$4xl"}>
+      <SizableText color={"$bg-brand-subtle-hover"} fontFamily="$OpenSansBold" size={"$4xl"}>
         Open Sans Bold - g
       </SizableText>
-      <SizableText fontFamily="$OpenSansBold" fontStyle="italic" size={"$4xl"}>
+      <SizableText
+        color={"$bg-success-strong"}
+        fontFamily="$OpenSansBold"
+        fontStyle="italic"
+        size={"$4xl"}
+      >
         Open Sans Bold Italic - g
       </SizableText>
     </View>
