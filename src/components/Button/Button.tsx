3d486a803003201273ca8a7a4ad872a3d11a1735
fabcodingzest <fabrizvi786@gmail.com>
2024-02-10 22:44:44 +0530

    testing tanagui in storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet, GestureResponderEvent } from "react-native";
+import { Button, H1, styled } from "tamagui";
+
+const CBtn = styled(Button, {
+  borderRadius: "$r-subtle",
+  backgroundColor: "$bg-error-strong",
+  color: "$text-invert",
+  alignSelf: "center",
+});
+
 type Props = {
   onPress: (event: GestureResponderEvent) => void;
   text: string;
 };
 
 export const MyButton = ({ onPress, text }: Props) => {
-  return (
-    <TouchableOpacity style={styles.container} onPress={onPress} activeOpacity={0.8}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
-  );
+  return <CBtn onPress={onPress}>{text}</CBtn>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    backgroundColor: "purple",
-    borderRadius: 8,
-  },
-  text: { color: "white" },
-});
