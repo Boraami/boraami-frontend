f6aa644a1b3b7a7ba4ae9bb3574747a1bfa0e7ac
cchow33 <carmendevelops@gmail.com>
2024-02-17 21:23:44 -0500

    add button

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 import { Button, H1, styled } from "tamagui";
-import { Alert } from '../src/components/Alert'
+import { Alert } from '../src/components/Alert/Alert'
 
 const CBtn = styled(Button, {
   borderRadius: "$r-strong",
@@ -14,7 +14,7 @@ export default function App() {
     <View>
       <H1>HOME</H1>
       <CBtn>Plain</CBtn>
-      <Alert/>
+      <Alert title="Delete Customer" content="This will remove all data relating to Alex. This action cannot be reversed. Deleted data can not be recovered."></Alert>
     </View>
   );
 }
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
-import { AlertDialog, styled } from 'tamagui'
+import { View, TouchableOpacity, Text, GestureResponderEvent} from 'react-native';
+import { AlertDialog, Button, styled } from 'tamagui';
 
-const WarningDialog = () => styled(AlertDialog, {
-
-
+const WarningDialog = styled(AlertDialog, {
+    width: '380',
+    backgroundColor: '#FFFFF',
+    borderColor: '$border-error-subtle'
 });
 
 type Props = {
-    onPress: (event: GestureResponderEvent) => void;
-    text: string;
+    title: string;
+    content: string;
 };
 
-export const Alert = ({ onPress, text }: Props) => {
-    return <WarningDialog onPress={ onPress }>{text}</WarningDialog>
+const Btn = styled(Button, {
+    borderRadius: "$r-subtle",
+    backgroundColor: "$bg-error-strong",
+    color: "$text-invert",
+    alignSelf: "center",
+});
+
+type BtnProps = {
+    onPress?: (event: GestureResponderEvent) => void;
+    text: string
+
+};
+
+export const Alert = ({ title, content}: Props) => {
+    return <WarningDialog><Button><h1>{title}</h1><p>{content}</p></Button></WarningDialog>;
 }
