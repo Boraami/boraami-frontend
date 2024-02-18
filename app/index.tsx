edf31ec5b7696fc6efd774feeb46aaa95057087e
cchow33 <carmendevelops@gmail.com>
2024-02-17 19:55:39 -0500

    add padding

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View } from "react-native";
 import Constants from "expo-constants";
 import Storybook from "../.storybook";
 import { Button, H1, styled } from "tamagui";
-import { StyledCard } from "../src/components/Card/Card";
+import { StreamerCard, StreamerBadge } from "../src/components/Card/Card";
 
 const CBtn = styled(Button, {
   borderRadius: "$r-strong",
@@ -13,11 +13,22 @@ const CBtn = styled(Button, {
 
 function App() {
   return (
-    <View style={{ padding: '8px' }}>
-      {/* <H1>HOME</H1> */}
-      {/* <CBtn>Plan</CBtn> */}
-      <StyledCard title="Playlist no.6" subtitle="btstatsreminder2" image="https://ichef.bbci.co.uk/news/1024/branded_news/160F5/production/_118775309_bts-16.jpg"></StyledCard>
+    <View>
+      <View style={{ padding: '8px' }}>
+        <View>
+        {/* <H1>HOME</H1> */}
+        {/* <CBtn>Plan</CBtn> */}
+        <StreamerCard title="Playlist no.6" subtitle="btstatsreminder2" image="https://ichef.bbci.co.uk/news/1024/branded_news/160F5/production/_118775309_bts-16.jpg"></StreamerCard>
+        </View>
+      </View>
+
+      <View style={{ padding: '8px'}}>
+        <View>
+        <StreamerBadge text="I earned the Dreamer Streamer badge for streaming 3 days in a row!" image="https://t3.ftcdn.net/jpg/05/63/73/08/240_F_563730870_ciwSsLDxuvUgsu8KYpyRG5J1MhVSVc11.jpg"></StreamerBadge>
+       </View>
+      </View>
     </View>
+
   );
 }
 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,37 +2,69 @@ import React from 'react'
 import { View } from 'react-native';
 import { Card, styled } from 'tamagui';
 import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 // Just work on light theme
 // Don't worry about passing props to Storybook component; just work on static values
 // Test components on Expo Go (Android) and XCode (iPhone)
 
-const CardComponent = styled(Card, {
+const StyledStreamerCard = styled(Card, {
     alignItems: 'flex-start',
     justifyContent: 'space-between',
     width: '100%',
     borderColor: '$border-error-subtle',
     borderTopWidth: 5,
+    borderTopColor: '$text-brand',
     backgroundColor: '$boraami.100',
     borderRadius: "$r-subtle",
     alignSelf: "center",
     display: 'flex', 
-    flexDirection: 'row'
+    flexDirection: 'row',
+    // font: '$text-primary'
 });
 
-type Props = {
+type StreamerCardProps = {
     title: string;
-    subtitle?: string;
-    image?: string; 
+    subtitle: string;
+    image: string; 
+}
+
+const StyledBadgeCard = styled(Card, {
+    width: '100%',
+    backgroundColor: '$boraami.100',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    display: 'flex',
+});
+
+type BadgeCardProps = {
+    text: string,
+    image: string;
 }
     
-export const StyledCard = ({ title, subtitle, image }: Props) => {
-    return <CardComponent>
+export const StreamerCard = ({ title, subtitle, image }: StreamerCardProps) => {
+    return (
+    <StyledStreamerCard>
         <img src={image} width="100"/>
-        <View style={{ flexGrow: 1, padding: 8}}><p>{title}</p><p>{subtitle}</p></View> 
-            <AntDesign name="close" size={24} color="black"/>
-        </CardComponent>
-};
+        <View style={{ flexGrow: 1, padding: 8 }}>
+            <View style={{ flexDirection: "row" }}>
+                <Entypo name="spotify" size={24} color="black" />
+                <p>{title}</p>
+            </View> 
+            <p>{subtitle}</p>
+        </View> 
+        <AntDesign name="close" size={24} color="black"/>
+    </StyledStreamerCard>
+)};
 
+export const StreamerBadge = ({ text, image }: BadgeCardProps) => {
+    return (
+    <StyledBadgeCard>
+        <View style={{ flexDirection: "row", marginHorizontal: 30 }}>
+        <img src={image} width="48"/>
+        <p style={{ padding: 12 }}>{text}</p>
+        </View>
+    </StyledBadgeCard>
+)};
 
 
