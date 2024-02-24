c873ecc48ea2703716abbcf97460683e5778e4f9
cchow33 <carmendevelops@gmail.com>
2024-02-24 09:53:26 -0500

    add imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,10 @@
 import Constants from "expo-constants";
 import Storybook from "../.storybook";
 import { View } from "tamagui";
-import { Alerts } from '../src/components/Alert/Alerts'
-import { WarningAlertDialog } from '../src/components/Alert/WarningAlertDialog'
 
 function App() {
-  return (
-    <View style={{ alignItems: 'center', paddingTop: 40}}>
-        <Alerts 
-          title='We are going live in July!'
-          color='#FFFFFF'
-          content='We are happy to announce that we are going live on 
-          July 28th. Get ready!'
-          backgroundColor='#5F3D9C'
-          size='long'
-          icon='heart'
-          iconColor='#FFFFFF'
-          closeColor='#FFFFFF'>
-        </Alerts>
-
-        {/* <WarningAlertDialog title="Dialogue" content="Dialogue Dialogue Dialogue "></WarningAlertDialog> */}
-    </View>)}
+  return ({})
+}
 
 export default Constants.expoConfig?.extra?.storybookEnabled ? 
 Storybook : App;
