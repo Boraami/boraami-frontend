5ab124b6eef224d61dd24195c29bec2be88f692c
cchow33 <carmendevelops@gmail.com>
2024-03-10 13:27:28 -0400

    add state to track characters

--- src/components/Textfields/Default.stories.tsx
+++ src/components/Textfields/Default.stories.tsx
@@ -12,6 +12,7 @@ export const TextfieldsMeta: Meta<typeof Textfields> = {
       defaultText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       fontSize: 14,
       helperText: 'Error Text',
+      maxLength: 1000
   },
 
     decorators: [
@@ -26,5 +27,25 @@ export const TextfieldsMeta: Meta<typeof Textfields> = {
 
 export default TextfieldsMeta;
 
-export const Default = {};
+export const Placeholder = {
+  name: 'placeholder',
+};
 
+export const Default = {
+  name: 'default',
+
+};
+
+export const Focused = {
+  name: 'focused',
+
+};
+
+export const Error = {
+  name: 'error',
+
+};
+
+export const Disabled = {
+  name: 'disabled',
+};
--- src/components/Textfields/Default.tsx
+++ src/components/Textfields/Default.tsx
@@ -1,52 +1,173 @@
-import React from 'react';
-import { XStack, View, YStack, ScrollView, Text } from 'tamagui';
-import { useColorScheme } from 'react-native';
-import { ScrollViewIndicator } from '@fanchenbao/react-native-scroll-indicator';
-import { colorScheme } from '../../themes/theme';
+import React, { useState } from "react";
+import {
+  XStack,
+  View,
+  YStack,
+  styled,
+  Label,
+  ScrollView,
+  TextArea,
+  Text,
+  SizableText,
+} from "tamagui";
+import { useColorScheme } from "react-native";
+// import { ScrollViewIndicator } from "@fanchenbao/react-native-scroll-indicator";
+import { colorScheme } from "../../themes/theme";
+
 
 type Props = {
-    num1: number,
-    num2: number,
-    helperText: string,
-    defaultText: string,
+  name: 'placeholder' | 'default' | 'focused' | 'error' | 'disabled';
+  helperText: string;
+  defaultText: string;
+  maxLength?: number;
+  onChange?: () => void;
 };
 
-    export const Textfields = ({ num1, num2, defaultText, helperText }: Props) => {
-       const theme = useColorScheme();
-       
-        return (
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  height: 136,
-                  width: 350,
-                  borderWidth: 1,
-                  borderRadius: 6,
-                  padding: 2, // padding issues . Without padding the border outline looks wierd, but pading creates white space...between the border and bakground fill ... 
-                  borderColor: colorScheme.boraami[300]
-                }}>
-                <View style={{ backgroundColor: theme === 'light' ? colorScheme.boraami[50] : colorScheme.boraami[900]}}>
-                    <ScrollViewIndicator 
-                        indStyle={{ 
-                            backgroundColor: theme === 'light' ? colorScheme.boraami[200] : colorScheme.boraami[500]
-                        }}> 
-                        <Text style={{ 
-                            color: theme === 'light' ? colorScheme.mono[800] : colorScheme.boraami[50] }}>{defaultText}
-                        </Text>
-                    </ScrollViewIndicator>
-                </View>
-                </View>
+const StyledTextbox = styled(TextArea, {
+  variants: {
+    placeholder: {
+      true: {
+        backgroundColor: "$placeholder-textbox-fill",
+        borderColor: "$placeholder-textbox-border",
+        textColor: "$placeholder-textbook-text",
+        helperTextColor: "$placeholder-helper-text",
+        scrollbarColor: "$placeholder-scroll-bar",
+      },
+    },
+    default: {
+      true: {
+        backgroundColor: "$default-textbox-fill",
+        borderColor: "$default-textbox-border",
+        textColor: "$default-textbook-text",
+        helperTextColor: "$default-helper-text",
+        scrollbarColor: "$default-scroll-bar",
+      },
+    },
+    focused: {
+      true: {
+        backgroundColor: "$focused-textbox-fill",
+        borderColor: "$focused-textbox-border",
+        textColor: "$focused-textbook-text",
+        helperTextColor: "$focused-helper-text",
+        scrollbarColor: "$focused-scroll-bar",
+      },
+    },
+    error: {
+      true: {
+        backgroundColor: "$error-textbox-fill",
+        borderColor: "$error-textbox-border",
+        textColor: "$error-textbook-text",
+        helperTextColor: "$error-helper-text",
+        scrollbarColor: "$error-scroll-bar",
+      },
+    },
+    disabled: {
+      true: {
+        backgroundColor: "$disabled-textbox-fill",
+        borderColor: "$disabled-textbox-border",
+        textColor: "$disabled-textbook-text",
+        helperTextColor: "$disabled-helper-text",
+        scrollbarColor: "$disabled-scroll-bar",
+      },
+    },
+  }
+})
 
-                <XStack width={350} paddingTop={3} flexDirection='row' justifyContent='space-between'>
-                    <Text style={{ color: theme === 'light' ? colorScheme.mono[600] : colorScheme.mono[300] }}>{helperText}</Text>
-                    <Text style={{ color: theme === 'light' ? colorScheme.mono[600] : colorScheme.mono[300] }}>{num1}/{num2}</Text>
-                </XStack> 
-            </View>
-          );
-        };
+export const Textfields = ({ name, maxLength, defaultText, helperText }: Props) => {
+  const theme = useColorScheme();
+  const [charCount, setCharCount] = useState(0);
 
+  const handleOnChangeText = (text: string) => {
+    setCharCount(text.length);
+  };
+
+  return (
+    <ScrollView maxHeight={161} borderColor={'orange'} borderWidth={2} paddingBottom={4}>
+      <TextArea
+          borderColor={colorScheme.boraami[300]}
+          height={136}
+          width={350}
+          backgroundColor={
+            theme === "light"
+              ? colorScheme.boraami[50]
+              : colorScheme.boraami[900]
+          }
+          borderRadius={6}
+          borderWidth={1}
+          onChangeText={handleOnChangeText}
+        >
+          <Text
+            fontFamily={"$body"}
+            fontSize={12}
+            lineHeight={18}
+            color={
+              theme === "light"
+                ? colorScheme.mono[800]
+                : colorScheme.boraami[50]
+            }
+          >
+            {defaultText}
+          </Text>
+        </TextArea>
+        <XStack flexDirection="row" justifyContent="space-between">
+          <Text
+            style={{
+              color:
+                theme === "light"
+                  ? colorScheme.mono[600]
+                  : colorScheme.mono[300],
+            }}
+          >
+            {helperText}
+          </Text>
+          <Text
+            style={{
+              color:
+                theme === "light"
+                  ? colorScheme.mono[600]
+                  : colorScheme.mono[300],
+            }}
+          >
+            {charCount}/{maxLength}
+          </Text>
+        </XStack>
+      {/* </YStack> */}
+    </ScrollView>
+  );
+};
+
+// return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//       <View
+//         style={{
+//           height: 136,
+//           width: 350,
+//           borderWidth: 1,
+//           borderRadius: 6,
+//           padding: 2, // padding issues . Without padding the border outline looks wierd, but pading creates white space...between the border and bakground fill ...
+//           borderColor: colorScheme.boraami[300]
+//         }}>
+//         <View style={{ backgroundColor: theme === 'light' ? colorScheme.boraami[50] : colorScheme.boraami[900]}}>
+//             <ScrollViewIndicator
+//                 indStyle={{
+//                     backgroundColor: theme === 'light' ? colorScheme.boraami[200] : colorScheme.boraami[500]
+//                 }}>
+//                 <Text style={{
+//                     color: theme === 'light' ? colorScheme.mono[800] : colorScheme.boraami[50] }}>{defaultText}
+//                 </Text>
+//             </ScrollViewIndicator>
+//         </View>
+//         </View>
+
+//         <XStack width={350} paddingTop={3} flexDirection='row' justifyContent='space-between'>
+//             <Text style={{ color: theme === 'light' ? colorScheme.mono[600] : colorScheme.mono[300] }}>{helperText}</Text>
+//             <Text style={{ color: theme === 'light' ? colorScheme.mono[600] : colorScheme.mono[300] }}>{num1}/{num2}</Text>
+//         </XStack>
+//     </View>
+//   );
+// };
