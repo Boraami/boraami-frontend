3ed926b55df3a5c3c5ae8f9f3ea088e515a559a3
nimra185 <nimrashabbir185@gmail.com>
2024-03-23 08:56:37 +0500

    overflow menu refactoring with fab

--- src/components/OverflowMenu/OverflowMenu.stories.tsx
+++ src/components/OverflowMenu/OverflowMenu.stories.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import { View } from "react-native";
 import { Meta } from "@storybook/react-native";
 import { OverflowMenu } from "./OverflowMenu";
+const overflowmenuItems = [
+  {
+    menuText: "Report ",
+    iconName:"flag",
+  },
+  {
+    menuText: "Mute user",
+    iconName:"bell-slash",
+  },
+  {
+    menuText: "Block user",
+    iconName:"user-slash",
+  },
+]
 const OverflowMenuMeta: Meta<typeof OverflowMenu> = {
     title: "OverflowMenu",
     component: OverflowMenu,
@@ -9,10 +23,7 @@ const OverflowMenuMeta: Meta<typeof OverflowMenu> = {
       
     },
     args: {
-      menutext1:'Report',
-      menutext2:'Mute user',
-      menutext3:'Block user',
-
+      data: overflowmenuItems,
     },
     decorators: [
       (Story) => (
--- src/components/OverflowMenu/OverflowMenu.tsx
+++ src/components/OverflowMenu/OverflowMenu.tsx
@@ -1,68 +1,68 @@
 import { useState } from "react";
 import { XStack, Button, SizableText } from "tamagui";
-import { FontAwesome6 } from '@expo/vector-icons';
-import { SimpleLineIcons } from '@expo/vector-icons';
+import { SimpleLineIcons } from "@expo/vector-icons";
+import Icon from "../Icon/Icon";
+
 type Props = {
-  menutext1: string,
-  menutext2: string,
-  menutext3: string,
-}
-export function OverflowMenu({ menutext1, menutext2, menutext3 }: Props){
-  const [visibility, setVisibility] = useState(false)
+  data: {
+    menuText: string;
+    iconName: string;
+  }[];
+};
+
+export function OverflowMenu({ data }: Props) {
+  const [visibility, setVisibility] = useState(false);
   const handleResponderRelease = () => {
-    setVisibility(!visibility)}
-/*upon use of component in a screen onStartShouldSetResponder={() => true} and onResponderRelease={handleResponderRelease}
+    setVisibility(!visibility);
+  };
+  /*upon use of component in a screen onStartShouldSetResponder={() => true} and onResponderRelease={handleResponderRelease}
 should be given to the parent component in order for the menu to disappear anywhere on the screen*/
-    return(
-      <XStack flexDirection='column' padding={2} 
-      onStartShouldSetResponder={() => true}
-      onResponderRelease={handleResponderRelease}>
-        <Button  alignSelf='flex-start' onPress={()=>setVisibility(!visibility)}>
-        <SimpleLineIcons name="options" size={24} color="#8F66D6"/>
+  return (
+    <>
+      <XStack
+        flexDirection="column"
+        padding={2}
+        onStartShouldSetResponder={() => true}
+        onResponderRelease={handleResponderRelease}
+      >
+        <Button alignSelf="flex-start" justifyContent='flex-start' width={80} height={20} onPress={() => setVisibility(!visibility)}>
+          <SimpleLineIcons name="options" size={24} color="#8F66D6" />
         </Button>
-        {visibility?
-         <XStack 
-         flexDirection="column" 
-         borderRadius={4} 
-         width={110}
-         paddingTop={9}
-         paddingBottom={10}
-         paddingRight={10}
-         paddingLeft={0}
-         height={90}
-         gap={6}
-         backgroundColor={"$overflow-bg-color"}>
-               <Button  justifyContent='flex-start' height={20} >
-                 <FontAwesome6 name="flag"  size={7} color="#F7F3FF" />
-                 <SizableText
-                  paddingTop={4}
-                  fontFamily={'$btn'}
-                  size={'$xs'}
-                  lineHeight={12}
-                  color={'$text-icon-color'}
-                  >{menutext1}</SizableText></Button>
-               <Button  justifyContent='flex-start' height={20} >
-                 <FontAwesome6 name="bell-slash" color="#F7F3FF" size={7} />
-                 <SizableText
-                  paddingTop={4}
-                  fontFamily={'$btn'}
-                  size={'$xs'}
-                  lineHeight={12}
-                  color={'$text-icon-color'}
-                  >{menutext2}</SizableText></Button>
-               <Button  justifyContent='flex-start' height={20} >
-                 <FontAwesome6 name="user-slash" color="#F7F3FF"size={7} />
-                 <SizableText
-                  paddingTop={4}
-                  fontFamily={'$btn'}
-                  size={'$xs'}
-                  lineHeight={12}
-                  color={'$text-icon-color'}
-                  >{menutext3}</SizableText></Button>
-         </XStack>
-      : null}
-        <XStack>
-        </XStack>
-        </XStack>
-)
+        {visibility ? (
+          <XStack
+            flexDirection="column"
+            borderRadius={4}
+            width={110}
+            paddingTop={9}
+            paddingBottom={10}
+            paddingRight={10}
+            paddingLeft={0}
+            gap={6}
+            backgroundColor={"$overflow-bg-color"}
+          >
+            {data.map((item, i) => {
+              {
+                return (
+                  <Button justifyContent="flex-start" height={20} key={`${item.menuText}-${i}`} paddingLeft={12}>
+                  <XStack width={12} height={12}>
+                    <Icon name={item.iconName} size={10} color="#F7F3FF" style={{}} />
+                  </XStack>
+                  <SizableText
+                    paddingTop={4}
+                    fontFamily={"$btn"}
+                    size={"$xs"}
+                    lineHeight={12}
+                    color={"$text-icon-color"}
+                  >
+                    {item.menuText}
+                  </SizableText>
+                </Button>
+                );
+              }
+            })}
+          </XStack>
+        ) : null}
+      </XStack>
+    </>
+  );
 }
