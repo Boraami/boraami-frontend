4da3218a1542fd13841730d9841812a4bef3ac18
fabcodingzest <fabrizvi786@gmail.com>
2024-07-04 17:15:10 +0530

    fixed: asset path and modified spinner duration

--- src/components/Spinner/Spinner.stories.tsx
+++ src/components/Spinner/Spinner.stories.tsx
@@ -4,45 +4,47 @@ import { Meta } from "@storybook/react-native";
 import { SpinnerComp } from "./Spinner";
 
 const SpinnerMeta: Meta<typeof SpinnerComp> = {
-    title: "Spinner",
-    component: SpinnerComp,
-    args: {
-      txt: 'small'
-    },
-    decorators: [
-      (Story) => (
-        <View style={{
-          alignItems: "center",
+  title: "Spinner",
+  component: SpinnerComp,
+  args: {
+    txt: "small",
+  },
+  decorators: [
+    (Story) => (
+      <View
+        style={{
           justifyContent: "center",
-          flex: 1, }}>
+          flex: 1,
+        }}
+      >
         <Story />
       </View>
-      ),
-    ],
-  };
+    ),
+  ],
+};
 
-  export default SpinnerMeta;
+export default SpinnerMeta;
 
-  export const spinner_xs = {
-    args: {
-      size: 'xs',
-    },
-  };
+export const spinner_xs = {
+  args: {
+    size: "xs",
+  },
+};
 
-  export const spinner_small = {
-    args: {
-      size: 'small',
-    },
-  };
+export const spinner_small = {
+  args: {
+    size: "sm",
+  },
+};
 
-  export const spinner_medium = {
-    args: {
-      size: 'medium',
-    },
-  };
+export const spinner_medium = {
+  args: {
+    size: "md",
+  },
+};
 
-  export const spinner_large = {
-    args: {
-      size: 'large',
-    },
-  };
+export const spinner_large = {
+  args: {
+    size: "lg",
+  },
+};
