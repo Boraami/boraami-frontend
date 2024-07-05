858cc799e62be9567a190ef9377a4ef226bb57aa
kigris <marguyne.koyayoro-fanne@epitech.eu>
2024-07-05 19:27:26 +0200

    notifications are now being passed individually

--- src/components/Notification/Notifications.tsx
+++ src/components/Notification/Notifications.tsx
@@ -1,86 +1,99 @@
 import React from "react";
-import { Avatar, ScrollView, Separator, SizableText, XStack, YStack } from "tamagui";
+import { XStack, YStack, SizableText, Separator } from "tamagui";
 import Icon from "../Icon/Icon";
 import Badge from "./Badges";
-import { SafeAreaView } from "react-native-safe-area-context";
 import QuotedPost from "../Post/QuotedPost";
+import { SafeAreaView, ScrollView } from "react-native";
+
 export type Props = {
-  data: {
-    iconName: "user-large" | "comment" | "retweet" | "heart";
-    displayName: string,
-    notificationType: string,
-    userName: string,
-    dateTime: string,
-    replyMsg: string,
-    quotedUsername: string,
-    quotedAvatarText: string,
-    quotedDisplayName: string,
-    quotedPostText: string,
-    quotedPostImg: string,
-  }[];
+  iconName: "user-large" | "comment" | "retweet" | "heart";
+  displayName: string,
+  notificationType: string,
+  userName: string,
+  dateTime: string,
+  replyMsg: string,
+  quotedUsername: string,
+  quotedAvatarText: string,
+  quotedDisplayName: string,
+  quotedPostText: string,
+  quotedPostImg: string,
 };
 
-export default function Notifications ({data}: Props){
+export default function Notifications({
+  iconName,
+  displayName,
+  notificationType,
+  userName,
+  dateTime,
+  replyMsg,
+  quotedUsername,
+  quotedAvatarText,
+  quotedDisplayName,
+  quotedPostText,
+  quotedPostImg,
+}: Props) {
   return (
     <SafeAreaView>
       <ScrollView>
-      {data.map((item, i) => {
-        return (
-        <XStack flexDirection="column" key={`${item.userName}${item.dateTime}-${i}`}>
-        <XStack
+    <XStack flexDirection="column">
+      <XStack
         gap={12}
         height={60}
         paddingTop={10}
         paddingBottom={10}
         paddingLeft={5}
         paddingRight={5}
-        >
-          <Icon
-            name={item.iconName}
-            size={17}
-            style={{
-              paddingTop: 8,
-            }}
-            color={'#AA7AFF'}
-          />
-          <YStack gap={4} width={'90%'}>
-            <XStack justifyContent='space-between' paddingTop={5}>
+      >
+        <Icon
+          name={iconName}
+          size={17}
+          style={{ paddingTop: 8 }}
+          color={'#AA7AFF'}
+        />
+        <YStack gap={4} width={'90%'}>
+          <XStack justifyContent='space-between' paddingTop={5}>
             <XStack>
               <SizableText
-              fontFamily={'$heading'}
-              color={'$username-action-taken-text'}
-              size={'$xs'}
-              paddingLeft={1}
-              paddingTop={1}>{item.displayName}</SizableText> 
+                fontFamily={'$heading'}
+                color={'$username-action-taken-text'}
+                size={'$xs'}
+                paddingLeft={1}
+                paddingTop={1}
+              >
+                {displayName}
+              </SizableText>
               <SizableText
-              fontFamily={'$body'}
-              size={'$xs'}
-              color={'$username-action-taken-text'}
-              paddingLeft={5}
-              wordWrap='normal'
-              >{item.notificationType}</SizableText>
+                fontFamily={'$body'}
+                size={'$xs'}
+                color={'$username-action-taken-text'}
+                paddingLeft={5}
+                wordWrap='normal'
+              >
+                {notificationType}
+              </SizableText>
             </XStack>
             <Badge color={'$boraami.700'} size="sm" count={0} />
-            </XStack>
-            <XStack
-            justifyContent='space-between'>
+          </XStack>
+          <XStack justifyContent='space-between'>
             <SizableText
-            fontFamily={'$body'}
-            size={'$xs'}
-            color={'$user-tag-text'}>
-            {item.userName}
+              fontFamily={'$body'}
+              size={'$xs'}
+              color={'$user-tag-text'}
+            >
+              {userName}
             </SizableText>
             <SizableText
-            fontFamily={'$body'}
-            size={'$2xs'}
-            color={'$date-time-text'}>
-            {item.dateTime}
+              fontFamily={'$body'}
+              size={'$2xs'}
+              color={'$date-time-text'}
+            >
+              {dateTime}
             </SizableText>
-            </XStack>
-          </YStack>
-        </XStack>
-        {item.replyMsg!== "" && (
-          <XStack>
+          </XStack>
+        </YStack>
+      </XStack>
+      {replyMsg !== "" && (
+        <XStack>
           <SizableText
             fontFamily={'$body'}
             size={'$sm'}
@@ -88,26 +101,26 @@ export default function Notifications ({data}: Props){
             paddingLeft={3}
             paddingBottom={15}
             wordWrap='normal'
-            selectable>{item.replyMsg}</SizableText>
-          </XStack>)}
-        {item.quotedDisplayName!==""&&(
-          <XStack paddingBottom={15}>
-            <QuotedPost 
-            quotedAvatarText={item.quotedAvatarText} 
-            quotedDisplayName={item.quotedDisplayName} 
-            quotedPostImg={item.quotedPostImg} 
-            quotedPostText={item.quotedPostText} 
-            quotedUsername={item.quotedUsername}/>
-            </XStack>
-        )}
-        <Separator borderColor={'$boraami.100'}/>
-      </XStack>
-      );
-      })}
-      
-      </ScrollView>
+            selectable
+          >
+            {replyMsg}
+          </SizableText>
+        </XStack>
+      )}
+      {quotedDisplayName !== "" && (
+        <XStack paddingBottom={15}>
+          <QuotedPost
+            quotedAvatarText={quotedAvatarText}
+            quotedDisplayName={quotedDisplayName}
+            quotedPostImg={quotedPostImg}
+            quotedPostText={quotedPostText}
+            quotedUsername={quotedUsername}
+          />
+        </XStack>
+      )}
+      <Separator borderColor={'$boraami.100'} />
+    </XStack>
+    </ScrollView>
     </SafeAreaView>
-  )
+  );
 };
-/*
-*/
