32389fcbc8cdeda16883fe5b556cf8f968d5daeb
fabcodingzest <fabrizvi786@gmail.com>
2024-07-09 02:07:24 +0530

    having props as optional in buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import {
   Button as Btn,
   ButtonProps as BtnProps,
-  XStack, SizableText, FontSizeTokens, styled
+  XStack,
+  SizableText,
+  FontSizeTokens,
+  styled,
 } from "tamagui";
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6 } from "@expo/vector-icons";
 
 interface CustomBtnProps extends BtnProps {
   name: string;
-  primary: string;
-  tertiary: string;
-  secondary: string;
-};
+  primary?: string;
+  tertiary?: string;
+  secondary?: string;
+}
 
-
-const Button: React.FC<CustomBtnProps> = (props) => <StyledBtn {...props} />
+const Button: React.FC<CustomBtnProps> = (props) => <StyledBtn {...props} />;
 
 type BtnSizeProps = {
   [key: string]: {
@@ -38,7 +40,7 @@ const btnSizes: BtnSizeProps = {
     left: 105,
     paddingHorizontal: 6,
     gap: -3,
-    txtSize: '$sm',
+    txtSize: "$sm",
     iconSize: 12,
     lineHeight: 12,
   },
@@ -49,7 +51,7 @@ const btnSizes: BtnSizeProps = {
     left: 813,
     paddingHorizontal: 6,
     gap: -1,
-    txtSize: '$md',
+    txtSize: "$md",
     iconSize: 16,
     lineHeight: 14,
   },
@@ -60,40 +62,40 @@ const btnSizes: BtnSizeProps = {
     left: 913,
     paddingHorizontal: 8,
     gap: 0,
-    txtSize: '$lg',
+    txtSize: "$lg",
     iconSize: 20,
     lineHeight: 18,
-  }
+  },
 };
 
 const StyledBtn = styled(Btn, {
-  borderRadius: '$r-subtle',
-  alignSelf: 'center',
-  justifyContent: 'center',
-  position: 'relative',
+  borderRadius: "$r-subtle",
+  alignSelf: "center",
+  justifyContent: "center",
+  position: "relative",
   paddingRight: 8,
   variants: {
     primary: {
       disabled: {
-        backgroundColor: '$primary-disabled-btn',
+        backgroundColor: "$primary-disabled-btn",
         borderWidth: 1.5,
         opacity: 1.5,
-        color: '$mono.100',
+        color: "$mono.100",
       },
       normal: {
-        backgroundColor: '$primary-default-btn',
+        backgroundColor: "$primary-default-btn",
         borderColor: "transparent",
         borderWidth: 2,
         hoverStyle: {
           backgroundColor: "$primary-hover-btn-fill",
-          shadowColor: '#0EA5E9',
+          shadowColor: "#0EA5E9",
           shadowOpacity: 1,
           shadowRadius: 12,
-          shadowOffset: { width: 1, height: 1 }
+          shadowOffset: { width: 1, height: 1 },
         },
         focusStyle: {
-          backgroundColor: '$primary-focus-btn-fill',
-          shadowColor: '#C2A0FF',
+          backgroundColor: "$primary-focus-btn-fill",
+          shadowColor: "#C2A0FF",
           shadowOpacity: 1,
           shadowRadius: 12,
           shadowOffset: { width: 1, height: 1 },
@@ -102,72 +104,70 @@ const StyledBtn = styled(Btn, {
     },
     secondary: {
       disabled: {
-        borderColor: '$secondary-disabled-btn-border',
+        borderColor: "$secondary-disabled-btn-border",
         borderWidth: 1,
       },
       normal: {
-        borderColor: '$secondary-default-btn-border',
+        borderColor: "$secondary-default-btn-border",
         borderWidth: 1,
         hoverStyle: {
           borderWidth: 1.5,
-          shadowColor: '#0EA5E9',
+          shadowColor: "#0EA5E9",
           shadowOpacity: 1,
           shadowRadius: 12,
-          shadowOffset: { width: 1, height: 1 }
+          shadowOffset: { width: 1, height: 1 },
         },
         focusStyle: {
           borderWidth: 25.5,
-          shadowColor: '#8F66D6',
+          shadowColor: "#8F66D6",
           shadowOpacity: 1,
           shadowRadius: 12,
           shadowOffset: { width: -5, height: 5 },
-        }
+        },
       },
     },
     tertiary: {
       disabled: {
-        borderRadius: '$r-sharp',
+        borderRadius: "$r-sharp",
         borderWidth: 1.5,
-        color: '#999999',
+        color: "#999999",
       },
       normal: {
         borderRadius: 0,
-        color: '$tertiary-default-text',
+        color: "$tertiary-default-text",
         borderBottomWidth: 2,
         hoverStyle: {
-          shadowColor: '#38BDF8',
+          shadowColor: "#38BDF8",
           borderBottomWidth: 2,
           shadowOpacity: 0.7,
           shadowRadius: 9,
-          shadowOffset: { width: 0, height: 7 }
+          shadowOffset: { width: 0, height: 7 },
         },
         focusStyle: {
-          shadowColor: '#C2A0FF',
+          shadowColor: "#C2A0FF",
           borderBottomWidth: 2,
           shadowOpacity: 1,
           shadowRadius: 9,
           shadowOffset: { width: 0, height: 7 },
-        }
+        },
       },
-    }
-  }
+    },
+  },
 });
 
-
-interface ButtonProps {
+export interface BtnFieldProps {
   txt: string;
   name: string;
-  label: string;
-  iconName: string;
+  iconName?: string;
   disabled?: boolean;
-  size: 'xs' | 'sm' | 'md';
-  primary: 'normal' | 'disabled';
-  iconPosition: 'left' | 'right';
-  tertiary: 'normal' | 'disabled';
-  secondary: 'normal' | 'disabled';
+  size: "xs" | "sm" | "md";
+  iconPosition?: "left" | "right";
+  primary?: "normal" | "disabled";
+  tertiary?: "normal" | "disabled";
+  secondary?: "normal" | "disabled";
 }
 
-export const BtnField = (props: ButtonProps) => {
+export const BtnField = (props: BtnFieldProps) => {
   const [isActive, setIsActive] = useState(false);
 
   const handlePressIn = () => {
@@ -186,37 +186,43 @@ export const BtnField = (props: ButtonProps) => {
     disabled,
     iconName,
     secondary,
-    iconPosition = 'left'
+    iconPosition = "left",
   } = props;
 
-
   const textColor = {
-    primary: disabled ? '#E9E5F0' : '#F7F3FF',
-    secondary: disabled ? '#999999' : '#AA7AFF',
-    tertiary: disabled ? '#999999' : '#AA7AFF'
-  }
-  const color = primary ? textColor.primary : secondary ? textColor.secondary : textColor.tertiary
+    primary: disabled ? "#E9E5F0" : "#F7F3FF",
+    secondary: disabled ? "#999999" : "#AA7AFF",
+    tertiary: disabled ? "#999999" : "#AA7AFF",
+  };
+  const color = primary ? textColor.primary : secondary ? textColor.secondary : textColor.tertiary;
 
   const bColor = {
-    primary: isActive ? '$boraami.400' : 'transparent',
-    secondary: isActive ? '$serendipity.500' : disabled ? '$secondary-disabled-btn-border' : '$secondary-default-btn-border',
-    tertiary: isActive ? '$boraami.500' : 'transparent',
-  }
-  const hoverColor = primary ? bColor.primary : secondary ? bColor.secondary : bColor.tertiary
+    primary: isActive ? "$boraami.400" : "transparent",
+    secondary: isActive
+      ? "$serendipity.500"
+      : disabled
+      ? "$secondary-disabled-btn-border"
+      : "$secondary-default-btn-border",
+    tertiary: isActive ? "$boraami.500" : "transparent",
+  };
+  const hoverColor = primary ? bColor.primary : secondary ? bColor.secondary : bColor.tertiary;
 
   const bWidth = {
     primary: isActive ? 1.5 : 1,
     secondary: isActive ? 1.5 : 1,
-    tertiary: isActive ? 1.5 : 1
-  }
+    tertiary: isActive ? 1.5 : 1,
+  };
 
-  const width = primary ? bWidth.primary : secondary ? bWidth.secondary : bWidth.tertiary
+  const width = primary ? bWidth.primary : secondary ? bWidth.secondary : bWidth.tertiary;
 
-  const iconComponent = <FontAwesome6 name={iconName}
-    size={btnSizes[size].iconSize}
-    color={color}
-    style={{ position: 'relative' }}
-  />;
+  const iconComponent = (
+    <FontAwesome6
+      name={iconName}
+      size={btnSizes[size].iconSize}
+      color={color}
+      style={{ position: "relative" }}
+    />
+  );
 
   return (
     <XStack>
@@ -228,24 +234,26 @@ export const BtnField = (props: ButtonProps) => {
         height={btnSizes[size].height}
         paddingHorizontal={btnSizes[size].paddingHorizontal}
         disabled={disabled}
-        primary={primary}
-        tertiary={tertiary}
-        secondary={secondary}
+        primary={primary ? primary : undefined}
+        tertiary={tertiary ? tertiary : undefined}
+        secondary={secondary ? secondary : undefined}
         borderWidth={width}
-        borderColor={primary || secondary ? hoverColor : 'transparent'}
-        borderBottomColor={tertiary ? hoverColor : 'transparent'}
+        borderColor={primary || secondary ? hoverColor : "transparent"}
+        borderBottomColor={tertiary ? hoverColor : "transparent"}
         icon={iconName ? iconComponent : null}
-        flexDirection={iconPosition === 'right' ? "row-reverse" : "row"}
+        flexDirection={iconPosition === "right" ? "row-reverse" : "row"}
       >
         <SizableText
-          fontFamily={'$btn'}
-          textAlign={'center'}
+          fontFamily={"$btn"}
+          textAlign={"center"}
           lineHeight={btnSizes[size].lineHeight}
           color={color}
           size={btnSizes[size].txtSize}
           top={0.1}
-        >{txt}</SizableText>
+        >
+          {txt}
+        </SizableText>
       </Button>
     </XStack>
-  )
-}
+  );
+};
