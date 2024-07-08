e09d275d599684b2d5b93044a39a9ad956958254
fabcodingzest <fabrizvi786@gmail.com>
2024-07-09 02:44:34 +0530

    Refactored for just avatar

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -4,76 +4,23 @@ import { Meta } from "@storybook/react-native";
 import AvatarUserProfile from "./Avatar";
 
 const AvatarUserProfileMeta: Meta<typeof AvatarUserProfile> = {
-    title: "AvatarUserProfile",
-    component: AvatarUserProfile,
-    argTypes: {
+  title: "AvatarUserProfile",
+  component: AvatarUserProfile,
+  argTypes: {},
+  args: {},
+  decorators: [
+    (Story) => (
+      <View style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+};
 
-    },
-    args: {
-    },
-    decorators: [
-      (Story) => (
-        <View>
-          <Story />
-        </View>
-      ),
-    ],
-  };
+export default AvatarUserProfileMeta;
 
-  export default AvatarUserProfileMeta;
-
-  export const AvatarUser1 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Yoongi’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser2 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Jimin’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser3 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Seokjin’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser4 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Namjoon’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser5 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Jungkook’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser6 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Taehyung’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
-
-  export const AvatarUser7 = {
-    args: {
-        AvatarText:'AA',
-        AvatarUsername:'Hobi’s wife',
-        UserTag: '@tangerines4life',
-    }
-  }
+export const AvatarUser1 = {
+  args: {
+    AvatarText: "FA",
+  },
+};
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,87 +1,23 @@
 import React from "react";
-import { Avatar, SizableText, XStack, Button, Separator, ScrollView } from "tamagui";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar, SizableText } from "tamagui";
 
 export type Props = {
-    AvatarUsername: string,
-    AvatarText: string,
-    UserTag: string,
+  AvatarText: string;
 };
 
-export default function AvatarUserProfile({ AvatarUsername, AvatarText, UserTag }: Props) {
-    return (
-        <SafeAreaView>
-            <ScrollView>
-                <XStack
-                    flexDirection="column"
-                    padding={8}
-                >
-                    <XStack
-                        flexDirection="row"
-                        justifyContent="space-between"
-                        gap={16}
-                        width={"100%"}
-                    >
-                        <XStack gap={5}>
-                            <Avatar
-                                circular
-                                backgroundColor={"$avatar-bg-color"}
-                                borderWidth={3}
-                                width={64}
-                                height={64}
-                                borderColor={"$avatar-border-color"}
-                            >
-                                <SizableText
-                                    fontFamily={"$btn"}
-                                    color={"$avatar-text"}
-                                    size={"$xl"}
-                                >
-                                    {AvatarText}
-                                </SizableText>
-                            </Avatar>
-                            <XStack flexDirection="column">
-                                <SizableText
-                                    fontFamily={"$heading"}
-                                    color={"$avatar-username"}
-                                    size={"$sm"}
-                                    paddingTop={8}
-                                >
-                                    {AvatarUsername}
-                                </SizableText>
-                                <SizableText
-                                    fontFamily={"$body"}
-                                    color={"$avatar-user-tag"}
-                                    size={"$xs"}
-                                >
-                                    {UserTag}
-                                </SizableText>
-                            </XStack>
-                        </XStack>
-                        <XStack paddingTop={12}>
-                            <Button
-                                width={46}
-                                height={21}
-                                borderRadius={4}
-                                borderColor={"$secondary-button-border"}
-                                paddingTop={3}
-                                paddingBottom={4}
-                                paddingLeft={4}
-                                paddingRight={4}
-                                gap={6}
-                            >
-                                <SizableText
-                                    fontFamily={"$btn"}
-                                    color={"$tertiary-default-text"}
-                                    size={"$xs"}
-                                >
-                                    Follow
-                                </SizableText>
-                            </Button>
-                        </XStack>
-                    </XStack>
-                    <Separator paddingTop={12} borderColor={"$avatar-divider"} />
-                </XStack>
-            </ScrollView>
-        </SafeAreaView>
-    )
+export default function AvatarUserProfile({ AvatarText }: Props) {
+  return (
+    <Avatar
+      circular
+      backgroundColor={"$avatar-bg-color"}
+      borderWidth={3}
+      width={64}
+      height={64}
+      borderColor={"$avatar-border-color"}
+    >
+      <SizableText fontFamily={"$btn"} color={"$avatar-text"} size={"$md"}>
+        {AvatarText}
+      </SizableText>
+    </Avatar>
+  );
 }
