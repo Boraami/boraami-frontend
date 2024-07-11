00121813a218e1f147770914d651e0811ee7e2c5
fabcodingzest <fabrizvi786@gmail.com>
2024-07-11 15:01:29 +0530

    modified for custom trigger and close btns and using reusable btn comp and fixed styling

--- src/components/Modals/ModalSingleActionButton.stories.tsx
+++ src/components/Modals/ModalSingleActionButton.stories.tsx
@@ -3,28 +3,34 @@ import { View } from "react-native";
 import { Meta } from "@storybook/react-native";
 import { ModalWithSingleActionButton } from "./ModalSingleActionButton";
 const ModalWithSingleActionButtonMeta: Meta<typeof ModalWithSingleActionButton> = {
-    title: "Modal With single Action Button",
-    component: ModalWithSingleActionButton,
-    argTypes: {
-      
+  title: "Modal With single Action Button",
+  component: ModalWithSingleActionButton,
+  argTypes: {},
+  args: {},
+  decorators: [
+    (Story) => (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+};
+
+export default ModalWithSingleActionButtonMeta;
+
+export const Modal1 = {
+  args: {
+    modeltitle: "Application Recieved",
+    btnText: "Alright",
+    btnName: "application-close",
+    btnStyles: {
+      txt: "Open Modal",
+      primary: "normal",
+      name: "application-close",
+      size: "md",
     },
-    args: {
-    },
-    decorators: [
-      (Story) => (
-        <View style={{alignItems: "center", justifyContent: "center", flex: 1 }}>
-          <Story />
-        </View>
-      ),
-    ],
-  };
-  
-  export default ModalWithSingleActionButtonMeta;
-  
-  export const Modal1 = {
-    args:{
-      modeltitle:'Application Recieved',
-      btntext:'Alright',
-      modeltext:'Your application has been received. We will review your application and respond within the next 48 hours.'
-    }
-  };
+    closeBtnText: "Alright",
+    modeltext:
+      "Your application has been received. We will review your application and respond within the next 48 hours.",
+  },
+};
--- src/components/Modals/ModalSingleActionButton.tsx
+++ src/components/Modals/ModalSingleActionButton.tsx
@@ -1,140 +1,151 @@
-import { XStack, SizableText, Image,} from "tamagui";
-import { Ionicons } from '@expo/vector-icons';
-import { Button,  Adapt, Dialog, Sheet } from 'tamagui'
-import {FontAwesome } from '@expo/vector-icons'
+import { XStack, SizableText, Image, YStack, Stack, View } from "tamagui";
+import { Ionicons } from "@expo/vector-icons";
+import { Adapt, Dialog, Sheet } from "tamagui";
+import { FontAwesome } from "@expo/vector-icons";
+import { BtnField, BtnFieldProps } from "../Button/Button";
+import { useState } from "react";
+import { GestureResponderEvent } from "react-native";
+
 type Props = {
-  modeltitle: string
-  modeltext: string, 
-  btntext: string
-}
-export function ModalWithSingleActionButton({ modeltext, modeltitle, btntext }: Props) {
-    return (
-      <Dialog modal>
-      <Dialog.Trigger asChild>
-        <Button>Show Modal With Single Action Button</Button>
-      </Dialog.Trigger>
-      
-      <Adapt when="sm" platform="touch">
-        <Sheet animation="medium" zIndex={200000} modal dismissOnSnapToBottom>
-          <Sheet.Frame padding="$2" gap="$4">
-            <Adapt.Contents />
-          </Sheet.Frame>
-          <Sheet.Overlay
-            animation="lazy"
+  modeltitle: string;
+  modeltext: string;
+  btnText: string;
+  closeBtnText: string;
+  btnStyles: BtnFieldProps;
+  onPress?: (...args: any[]) => void; // since there could be any action attached to btn
+};
+
+export function ModalWithSingleActionButton({
+  modeltext,
+  modeltitle,
+  closeBtnText,
+  btnStyles,
+  onPress,
+}: Props) {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const handleCloseBtn = () => {
+    onPress && onPress();
+    setDialogOpen(false);
+  };
+
+  return (
+    <View>
+      <BtnField {...btnStyles} onPress={() => setDialogOpen(true)} />
+      <Dialog modal open={isDialogOpen} onOpenChange={setDialogOpen}>
+        <Adapt when="sm" platform="touch">
+          <Sheet animation="medium" zIndex={200000} modal dismissOnSnapToBottom>
+            <Sheet.Frame padding="$2" gap="$4">
+              <Adapt.Contents />
+            </Sheet.Frame>
+            <Sheet.Overlay
+              animation="lazy"
+              enterStyle={{ opacity: 0 }}
+              exitStyle={{ opacity: 0 }}
+            />
+          </Sheet>
+        </Adapt>
+        <Dialog.Portal>
+          <Dialog.Overlay
+            key="overlay"
+            animation="slow"
+            opacity={0.5}
             enterStyle={{ opacity: 0 }}
             exitStyle={{ opacity: 0 }}
           />
-        </Sheet>
-      </Adapt>
-
-      <Dialog.Portal>
-        <Dialog.Overlay
-          key="overlay"
-          animation="slow"
-          opacity={0.5}
-          enterStyle={{ opacity: 0 }}
-          exitStyle={{ opacity: 0 }}
-        />
-
-        <Dialog.Content
-          bordered
-          elevate
-          key="content"
-          animateOnly={['transform', 'opacity']}
-          animation={[
-            'quick',
-            {
-              opacity: {
-                overshootClamping: true,
+          <Dialog.Content
+            bordered
+            elevate
+            key="content"
+            animateOnly={["transform", "opacity"]}
+            animation={[
+              "quick",
+              {
+                opacity: {
+                  overshootClamping: true,
+                },
               },
-            },
-          ]}
-          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-          gap="$4"
-        >
-          <XStack
-          flexDirection="column">
-            <Image
-              source={{
-                      uri: require('../../media/images/sitting1.png'),
-              }}
-              width={59}
-              height={51}
-              resizeMode={'contain'}
-              alignSelf='flex-end'
-            />
-            <XStack
-            backgroundColor={'$error-alert-fill'}
-            width={350}
-            height={113}
-            borderTopLeftRadius={8}
-            borderTopRightRadius={8}
-            padding={16}
-            justifyContent="center"
-            >
-              <XStack 
-              flexDirection="row"
-              alignItems='center'
-              justifyContent="center"
-              alignSelf="center"
-              gap={10}>
-             <Ionicons name="checkmark-circle"  size={22} color="#2F9D83" />
-              <SizableText 
-              fontFamily={'$heading'}
-              paddingTop={1}
-              lineHeight={25}
-              size={'$lg'}
-              color="$title-text" >{modeltitle}</SizableText>
-          </XStack>
-        </XStack>
-        <XStack
-        backgroundColor={'$error-alert-fill'}
-        width={350}
-        height={70}
-        paddingLeft={20}
-        paddingRight={20}
-        paddingBottom={16}>
-           <SizableText 
-            fontFamily={'$body'}
-            size={'$md'}
-            lineHeight={21}
-            color="$supporting-text" >{modeltext}</SizableText>
-        </XStack>
-        <XStack
-          backgroundColor={'$error-alert-fill'}
-          width={350}
-          height={72}
-          borderBottomLeftRadius={8}
-          borderBottomRightRadius={8}
-          paddingTop={16}
-          paddingLeft={20}
-          paddingRight={20}
-          paddingBottom={16}>
-          <Dialog.Close displayWhenAdapted  asChild>
-          <Button
-            width={310}
-            height={40}
-            borderRadius={4}
-            paddingTop={3}
-            backgroundColor={"$primary-button-fill"}
-            justifyContent='center'
-            alignItems='center'
-            gap={-4}
+            ]}
+            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+            gap="$4"
           >
-          <SizableText
-          color='$boraami.50'
-          size='$lg'
-          lineHeight={22}
-          fontFamily='$btn'
-          >{btntext}</SizableText>
-          <FontAwesome name="heart" size={14} paddingBottom={3} color="#F7F3FF" /></Button>
-          </Dialog.Close>
-        </XStack>
-        </XStack>
-        </Dialog.Content>
-      </Dialog.Portal>
-  </Dialog>
-    )
-  }
-  
+            <Stack width={"100vw"} justifyContent="center" alignItems="center">
+              <YStack width={350}>
+                <Image
+                  source={{
+                    uri: require("../../media/images/sitting1.png"),
+                  }}
+                  width={59}
+                  height={51}
+                  alignSelf="flex-end"
+                />
+                <XStack
+                  backgroundColor={"$error-alert-fill"}
+                  width={350}
+                  borderTopLeftRadius={8}
+                  borderTopRightRadius={8}
+                  paddingVertical={28}
+                  justifyContent="center"
+                >
+                  <XStack
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center"
+                    alignSelf="center"
+                    gap={10}
+                  >
+                    <Ionicons name="checkmark-circle" size={22} color="#2F9D83" />
+                    <SizableText
+                      fontFamily={"$heading"}
+                      paddingTop={1}
+                      size={"$lg"}
+                      color="$title-text"
+                    >
+                      {modeltitle}
+                    </SizableText>
+                  </XStack>
+                </XStack>
+                <XStack
+                  backgroundColor={"$error-alert-fill"}
+                  paddingHorizontal={20}
+                  paddingBottom={16}
+                >
+                  <SizableText
+                    fontFamily={"$body"}
+                    size={"$md"}
+                    lineHeight={21}
+                    color="$supporting-text"
+                  >
+                    {modeltext}
+                  </SizableText>
+                </XStack>
+                <XStack
+                  backgroundColor={"$error-alert-fill"}
+                  width={350}
+                  height={72}
+                  borderBottomLeftRadius={8}
+                  borderBottomRightRadius={8}
+                  paddingTop={16}
+                  paddingLeft={20}
+                  paddingRight={20}
+                  paddingBottom={16}
+                >
+                  <BtnField
+                    txt={closeBtnText}
+                    size="lg"
+                    name="dialog-close"
+                    width="100%"
+                    primary="normal"
+                    iconFromParent={<FontAwesome name="heart" size={14} color="#F7F3FF" />}
+                    onPress={handleCloseBtn}
+                  />
+                </XStack>
+              </YStack>
+            </Stack>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog>
+    </View>
+  );
+}
