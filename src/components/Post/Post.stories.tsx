f8bbc0f04fd400541af5b49c0060444ab9b6c8b5
fabcodingzest <fabrizvi786@gmail.com>
2024-07-27 04:14:59 +0530

    renamed quoted post to post

---
+++ src/components/Post/Post.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View } from "react-native";
+import { Meta } from "@storybook/react-native";
+import Post from "./Post";
+const PostMeta: Meta<typeof Post> = {
+  title: "Post",
+  component: Post,
+  argTypes: {},
+  args: {
+    avatarText: "TT",
+    displayName: "Bessie Cooper",
+    username: "@armyuser1",
+    postText: "I miss them.",
+    postImg: require("../../assets/Notification/Image.png"),
+  },
+  decorators: [
+    (Story) => (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+};
+
+export default PostMeta;
+
+export const TimelinePost = {};
