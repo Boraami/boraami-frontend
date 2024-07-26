22c39862178abfcd8a8fcaa4cbff9a562e47029d
fabcodingzest <fabrizvi786@gmail.com>
2024-07-27 03:29:36 +0530

    Modified tags to toggle solid-outline state

--- src/components/Tags/Tags.stories.tsx
+++ src/components/Tags/Tags.stories.tsx
@@ -4,120 +4,92 @@ import { Meta } from "@storybook/react-native";
 import { TagField } from "./Tags";
 
 const TagMeta: Meta<typeof TagField> = {
-    title: "Tags",
-    component: TagField,
-    argTypes: {},
-    decorators: [
-        (Story) => (
-            <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-            }}>
-                <Story />
-            </View>
-        ),
-    ],
+  title: "Tags",
+  component: TagField,
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
+        }}
+      >
+        <Story />
+      </View>
+    ),
+  ],
 };
 
 export default TagMeta;
 
-export const info_small = {
-    args: {
-        status: 'info',
-        state: 'solid',
-        size: 'sm',
-        txt: 'INFO',
-        showIcon: false,
-    },
+export const info_md = {
+  args: {
+    status: "info",
+    state: "solid",
+    size: "md",
+    txt: "INFO",
+    showIcon: true,
+  },
 };
 
 export const success_small = {
-    args: {
-        status: 'success',
-        state: 'solid',
-        size: 'sm',
-        txt: 'SUCCESS',
-        showIcon: false,
-    },
+  args: {
+    status: "success",
+    state: "solid",
+    size: "sm",
+    txt: "SUCCESS",
+    showIcon: false,
+  },
 };
 
-export const error_small = {
-    args: {
-        status: 'error',
-        state: 'solid',
-        size: 'sm',
-        txt: 'ERROR',
-        showIcon: false,
-    },
+export const error_md = {
+  args: {
+    status: "error",
+    state: "solid",
+    size: "md",
+    txt: "ERROR",
+    showIcon: true,
+  },
 };
 
 export const warning_small = {
-    args: {
-        status: 'warning',
-        state: 'solid',
-        size: 'sm',
-        txt: 'WARNING',
-        showIcon: false,
-    },
+  args: {
+    status: "warning",
+    state: "solid",
+    size: "sm",
+    txt: "WARNING",
+    showIcon: true,
+  },
 };
 
 export const default_small = {
-    args: {
-        status: 'social',
-        state: 'solid',
-        size: 'sm',
-        txt: 'DEFAULT',
-        showIcon: false,
-    }
+  args: {
+    status: "social",
+    state: "solid",
+    size: "sm",
+    txt: "DEFAULT",
+    showIcon: true,
+  },
 };
 
-export const info_medium_out = {
-    args: {
-        status: 'info',
-        state: 'outline',
-        size: 'md',
-        txt: 'INFO',
-        showIcon: true
-    },
+export const smDefaultNoIcon = {
+  args: {
+    status: "social",
+    state: "solid",
+    size: "sm",
+    txt: "DEFAULT",
+    showIcon: false,
+  },
 };
 
-export const success_medium_out = {
-    args: {
-        status: 'success',
-        state: 'outline',
-        showIcon: true,
-        size: 'md',
-        txt: 'SUCCESS',
-    },
+export const mdSuccessNoIcon = {
+  args: {
+    status: "social",
+    state: "solid",
+    size: "md",
+    txt: "DEFAULT",
+    showIcon: false,
+  },
 };
-
-export const error_medium_out = {
-    args: {
-        status: 'error',
-        state: 'outline',
-        size: 'md',
-        txt: 'ERROR',
-        showIcon: true
-    },
-};
-
-export const warning_medium_out = {
-    args: {
-        status: 'warning',
-        state: 'outline',
-        size: 'md',
-        txt: 'WARNING',
-        showIcon: true
-    },
-};
-
-export const default_medium_out = {
-    args: {
-        status: 'social',
-        state: 'outline',
-        size: 'md',
-        txt: 'DEFAULT',
-        showIcon: true
-    }
-}
--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -1,162 +1,168 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-    Button as Btn,
-    ButtonProps as BtnProps,
-    SizableText,
-    FontSizeTokens,
-    styled
+  Button as Btn,
+  ButtonProps as BtnProps,
+  SizableText,
+  FontSizeTokens,
+  styled,
 } from "tamagui";
-import {
-    FontAwesome6,
-} from '@expo/vector-icons';
+import { FontAwesome6 } from "@expo/vector-icons";
 
 interface CustomTagProps extends BtnProps {
-    status: 'info' | 'success' | 'error' | 'warning' | 'social';
-    state: 'solid' | 'outline';
-};
+  status: "info" | "success" | "error" | "warning" | "social";
+  state: "solid" | "outline";
+}
 
-const Button: React.FC<CustomTagProps> = (props) => <StyledTag {...props} />
+const Button: React.FC<CustomTagProps> = (props) => <StyledTag {...props} />;
 
 type TagSizeProps = {
-    [key: string]: {
-        height: number;
-        txtSize: FontSizeTokens;
-        iconSize: number;
-    };
+  [key: string]: {
+    height: number;
+    txtSize: FontSizeTokens;
+    iconSize: number;
+  };
 };
 
 const tagSizes: TagSizeProps = {
-    sm: {
-        height: 20,
-        txtSize: '$xs',
-        iconSize: 12,
-    },
-    md: {
-        height: 24,
-        txtSize: '$sm',
-        iconSize: 12,
-    }
+  sm: {
+    height: 20,
+    txtSize: "$xs",
+    iconSize: 12,
+  },
+  md: {
+    height: 24,
+    txtSize: "$sm",
+    iconSize: 12,
+  },
 };
 
 const StyledTag = styled(Btn, {
-    borderRadius: 4,
-    paddingLeft: 6,
-    paddingRight: 6,
-    alignItems: 'center',
-    variants: {
-        status: {
-            info: {
-                solid: {
-                    backgroundColor: '$info-solid-fill',
-                    color: '$info-solid-text',
-                },
-                outline: {
-                    borderColor: '$info-outline-border',
-                    color: '$info-outline-text',
-                },
-            },
-            success: {
-                solid: {
-                    backgroundColor: '$success-solid-fill',
-                    color: '$success-solid-text',
-                },
-                outline: {
-                    borderColor: '$success-outline-border',
-                    color: '$success-outline-text',
-                },
-            },
-            error: {
-                solid: {
-                    backgroundColor: '$error-solid-fill',
-                    color: '$error-solid-text',
-                },
-                outline: {
-                    borderColor: '$error-outline-border',
-                    color: '$error-outline-text',
-                },
-            },
-            warning: {
-                solid: {
-                    backgroundColor: '$warning-solid-fill',
-                    color: '$warning-solid-text',
-                },
-                outline: {
-                    borderColor: '$warning-outline-border',
-                    color: '$warning-outline-text',
-                },
-            },
-            social: {
-                solid: {
-                    backgroundColor: '$default-solid-fill',
-                    color: '$default-solid-text',
-                },
-                outline: {
-                    borderColor: '$default-outline-border',
-                    color: '$default-outline-text',
-                },
-            },
+  borderRadius: 4,
+  paddingLeft: 6,
+  paddingRight: 6,
+  alignItems: "center",
+  variants: {
+    status: {
+      info: {
+        solid: {
+          backgroundColor: "$info-solid-fill",
+          color: "$info-solid-text",
         },
+        outline: {
+          borderColor: "$info-outline-border",
+          color: "$info-outline-text",
+        },
+      },
+      success: {
+        solid: {
+          backgroundColor: "$success-solid-fill",
+          color: "$success-solid-text",
+        },
+        outline: {
+          borderColor: "$success-outline-border",
+          color: "$success-outline-text",
+        },
+      },
+      error: {
+        solid: {
+          backgroundColor: "$error-solid-fill",
+          color: "$error-solid-text",
+        },
+        outline: {
+          borderColor: "$error-outline-border",
+          color: "$error-outline-text",
+        },
+      },
+      warning: {
+        solid: {
+          backgroundColor: "$warning-solid-fill",
+          color: "$warning-solid-text",
+        },
+        outline: {
+          borderColor: "$warning-outline-border",
+          color: "$warning-outline-text",
+        },
+      },
+      social: {
+        solid: {
+          backgroundColor: "$default-solid-fill",
+          color: "$default-solid-text",
+        },
+        outline: {
+          borderColor: "$default-outline-border",
+          color: "$default-outline-text",
+        },
+      },
     },
+  },
 });
 
 interface ButtonProps {
-    txt: string;
-    status: 'info' | 'success' | 'error' | 'warning' | 'social';
-    state: 'solid' | 'outline';
-    size: 'sm' | 'md';
-    showIcon: boolean;
+  txt: string;
+  status: "info" | "success" | "error" | "warning" | "social";
+  state: "solid" | "outline";
+  size: "sm" | "md";
+  showIcon: boolean;
+  handleAction?: (...args: any[]) => void; // since there could be any action attached to btn
 }
 
 export const TagField: React.FC<ButtonProps> = (props) => {
-    const {
-        txt,
-        size,
-        status,
-        state,
-        showIcon
-    } = props;
+  const { txt, size, status, state, showIcon, handleAction } = props;
+  const [stateVar, setStatusVar] = useState(state);
 
-    const statusColor =  (status === 'info' ? '#0284C7' :
-    status === 'success' ? '#27846E' :
-        status === 'error' ? '#DB2777' :
-            status === 'warning' ? '#B95D29' : '#744FB5');
+  const statusColor =
+    status === "info"
+      ? "#0284C7"
+      : status === "success"
+      ? "#27846E"
+      : status === "error"
+      ? "#DB2777"
+      : status === "warning"
+      ? "#B95D29"
+      : "#744FB5";
 
-const iconColor = state === "solid" ? "white" : statusColor;
+  const iconColor = stateVar === "solid" ? "white" : statusColor;
 
-const borColor = state === "outline" ? "white" : statusColor;
+  const borColor = stateVar === "outline" ? "white" : statusColor;
 
-    const iconMap: { [key: string]: string } = {
-        info: "lightbulb",
-        success: "check",
-        error: "triangle-exclamation",
-        warning: "triangle-exclamation",
-        social: "heart",
-    };
+  const iconMap: { [key: string]: string } = {
+    info: "lightbulb",
+    success: "check",
+    error: "triangle-exclamation",
+    warning: "triangle-exclamation",
+    social: "heart",
+  };
 
-    const iconComponent = <FontAwesome6
-        name={iconMap[status]}
-        size={tagSizes[size].iconSize}
+  const iconComponent = (
+    <FontAwesome6 name={iconMap[status]} size={tagSizes[size].iconSize} color={iconColor} />
+  );
+
+  const handleActionPress = () => {
+    // Do anything else you wish on component level
+    setStatusVar((prevState) => (prevState === "solid" ? "outline" : "solid"));
+    handleAction && handleAction();
+  };
+
+  return (
+    <Button
+      status={status}
+      height={tagSizes[size].height}
+      state={stateVar}
+      borderRadius={4}
+      borderColor={stateVar === "outline" ? iconColor : "transparent"}
+      backgroundColor={stateVar === "solid" ? borColor : "transparent"}
+      icon={showIcon ? iconComponent : null}
+      scaleSpace={0.4}
+      onPress={handleActionPress}
+    >
+      <SizableText
+        fontFamily={"$btn"}
+        size={tagSizes[size].txtSize}
+        fontWeight={"500"}
         color={iconColor}
-    />
-
-    return (
-        <Button
-            status={status}
-            height={tagSizes[size].height}
-            disabled={true}
-            state={state}
-            borderRadius={4}
-            borderColor={state === 'outline' ? iconColor : 'transparent'}
-            backgroundColor={state === 'solid' ? borColor : 'transparent'}
-            icon={showIcon ? iconComponent : null}
-            scaleSpace={0.4}
-        >
-            <SizableText
-                fontFamily={"$btn"}
-                size={tagSizes[size].txtSize}
-                fontWeight={'500'}
-                color={iconColor}>{txt}
-            </SizableText>
-        </Button>
-    );
+      >
+        {txt}
+      </SizableText>
+    </Button>
+  );
 };
