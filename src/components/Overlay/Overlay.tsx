6737b4e14f49924b70c3fd91038752d5ae1ff4fd
fabcodingzest <fabrizvi786@gmail.com>
2024-07-27 18:22:09 +0530

    formatted and yarn update

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,12 +1,14 @@
-import { XStack} from "tamagui";
-import { Badge } from "../Badge/Badge";
-export function Overlay(){
-    return(
-        <XStack 
-        width={"100%"}
-        height={"100%"}
-        opacity={0.4}
-        backgroundColor={"$overlay-bg-color"}>
-        </XStack>
-    )
-}
+import { XStack } from "tamagui";
+
+const Overlay = () => {
+  return (
+    <XStack
+      width={"100%"}
+      height={"100%"}
+      opacity={0.4}
+      backgroundColor={"$overlay-bg-color"}
+    ></XStack>
+  );
+};
+
+export default Overlay;
