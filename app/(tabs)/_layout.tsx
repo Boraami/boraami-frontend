10214c6ed24511a29bea16d8b35f0fcf150cab8c
fabcodingzest <fabrizvi786@gmail.com>
2024-09-21 01:00:18 +0530

    added CustomTabIconButton and active states

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,30 +2,47 @@ import { useState } from "react";
 import { Tabs, useNavigation } from "expo-router";
 import { Image, Dimensions, View, useColorScheme, TouchableOpacity } from "react-native";
 import { colorScheme } from "../../src/themes/theme";
-import { DrawerActions} from '@react-navigation/native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { DrawerActions } from "@react-navigation/native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Button, Stack } from "tamagui";
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 
 function LogoTitle() {
   const theme = useColorScheme();
   const isDarkTheme = theme === "dark";
-  const iconTheme = isDarkTheme ?
-      require('../../assets/dark.png') :
-      require('../../assets/light.png');
+  const iconTheme = isDarkTheme
+    ? require("../../assets/dark.png")
+    : require("../../assets/light.png");
   return (
-      <View>
-          <Image
-              style={{
-                alignSelf: 'flex-end',
-                marginRight: 24,
-                marginTop: 7,
-                width: 101,
-                height: 48 }}
-              source={iconTheme}
-          />
-      </View>
+    <View>
+      <Image
+        style={{
+          alignSelf: "flex-end",
+          marginRight: 24,
+          marginTop: 7,
+          width: 101,
+          height: 48,
+        }}
+        source={iconTheme}
+      />
+    </View>
   );
 }
 
+const CustomTabIconButtons = (props: BottomTabBarButtonProps) => (
+  <TouchableOpacity
+    activeOpacity={1}
+    {...props}
+    style={{
+      width: "25%",
+      padding: 0,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    {props.children}
+  </TouchableOpacity>
+);
 
 export default function TabsLayout() {
   const theme = useColorScheme();
@@ -36,7 +53,7 @@ export default function TabsLayout() {
   const dividerColor = isDarkTheme ? colorScheme.boraami[600] : colorScheme.boraami[300];
   const notifDotColor = colorScheme.butter[400];
   const [isActive, setIsActive] = useState(false);
-  const barColor = isDarkTheme ? '#140233' : '#F7F3FF';
+  const barColor = isDarkTheme ? "#140233" : "#F7F3FF";
 
   const handleNotificationactive = () => {
     setIsActive(true);
@@ -45,82 +62,134 @@ export default function TabsLayout() {
   const handleNotificationInactive = () => {
     setIsActive(false);
   };
-  const notifBag = isActive ? notifDotColor : 'transparent';
+  const notifBag = isActive ? notifDotColor : "transparent";
   const navigate = useNavigation();
   console.log();
   return (
-    <Tabs screenOptions={{
-      headerShown: true,
+    <Tabs
+      screenOptions={{
+        headerShown: true,
         headerRight: () => <LogoTitle />,
         headerStyle: {
-            backgroundColor: barColor,
-            borderBottomColor: dividerColor,
-            borderBottomWidth: 0.5
+          backgroundColor: barColor,
+          borderBottomColor: dividerColor,
+          borderBottomWidth: 0.5,
         },
-      tabBarShowLabel: false,
-      tabBarActiveTintColor: activeIconColor,
-      tabBarInactiveTintColor: defaultIconColor,
-      tabBarStyle: {
-        height: 56,
-        backgroundColor: navBarColor, //boraami.50
-        borderTopColor: dividerColor,
-      },
-    }}>
-      <Tabs.Screen name="home/index"
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: activeIconColor,
+        tabBarInactiveTintColor: defaultIconColor,
+        tabBarStyle: {
+          // height: 56, see if this works on android
+          backgroundColor: navBarColor, //boraami.50
+          borderTopColor: dividerColor,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home/index"
         options={{
-          tabBarLabel: 'Home',
-          title: 'Home',
+          tabBarLabel: "Home",
+          title: "Home",
           tabBarShowLabel: false,
+          tabBarButton: (props) => <CustomTabIconButtons {...props} />,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" size={24} color={color} />)
-        }} />
-      <Tabs.Screen name="search/index"
+            <Stack
+              borderTopColor={color === activeIconColor ? activeIconColor : "transparent"}
+              borderTopWidth={3}
+              width={23}
+              height={40}
+              jc="center"
+              position="absolute"
+              top={0}
+              ai="center"
+            >
+              <MaterialIcons name="home" size={24} color={color} />
+            </Stack>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search/index"
         options={{
-          tabBarLabel: 'Search',
-          title: 'Search',
+          tabBarLabel: "Search",
+          title: "Search",
           tabBarShowLabel: false,
+
+          tabBarButton: (props) => <CustomTabIconButtons {...props} />,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="search" size={24} color={color} />)
-        }} />
-      <Tabs.Screen name="notification/index"
+            <Stack
+              borderTopColor={color === activeIconColor ? activeIconColor : "transparent"}
+              borderTopWidth={3}
+              width={23}
+              height={40}
+              jc="center"
+              position="absolute"
+              top={0}
+              ai="center"
+            >
+              <MaterialIcons name="search" size={24} color={color} />
+            </Stack>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notification/index"
         options={{
-          tabBarLabel: 'Notification',
-          title: 'Notification',
+          tabBarLabel: "Notification",
+          title: "Notification",
           tabBarShowLabel: false,
-          tabBarBadge: '',
+          tabBarBadge: "",
           tabBarBadgeStyle: {
             backgroundColor: notifDotColor,
             top: 4,
             left: 1,
           },
+          tabBarButton: (props) => <CustomTabIconButtons {...props} />,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="notifications" size={24} color={color} />)
-        }} />
-      <Tabs.Screen name="drawer/index"
+            <Stack
+              borderTopColor={color === activeIconColor ? activeIconColor : "transparent"}
+              borderTopWidth={3}
+              width={23}
+              height={40}
+              jc="center"
+              position="absolute"
+              top={0}
+              ai="center"
+            >
+              <MaterialIcons name="notifications" size={24} color={color} />
+            </Stack>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="drawer/index"
         options={{
-          title: 'Drawer',
+          title: "Drawer",
           tabBarShowLabel: false,
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="menu" size={24} color={color} />),
-            tabBarButton: (props) => (
-              <TouchableOpacity
-               {...props} onPress={() => navigate.dispatch(DrawerActions.openDrawer())}/>
-            )
-        }} />
-        <Tabs.Screen name="(drawer)/coffee/index" options={{title: 'Coffee', href: null}}/>
-        <Tabs.Screen name="(drawer)/conduct/index" options={{title: 'Conduct',href: null}}/>
-        <Tabs.Screen name="(drawer)/profile/index" options={{title: 'Profile',href: null}}/>
-        <Tabs.Screen name="(drawer)/terms/index" options={{title: 'Terms',href: null}}/>
+          tabBarIcon: ({ color }) => <MaterialIcons name="menu" size={24} color={color} />,
+          tabBarButton: (props) => (
+            <Button width="25%" onPress={() => navigate.dispatch(DrawerActions.openDrawer())}>
+              <Stack justifyContent="center" alignItems="center">
+                {props.children}
+              </Stack>
+            </Button>
+          ),
+        }}
+      />
+      <Tabs.Screen name="(drawer)/coffee/index" options={{ title: "Coffee", href: null }} />
+      <Tabs.Screen name="(drawer)/conduct/index" options={{ title: "Conduct", href: null }} />
+      <Tabs.Screen name="(drawer)/profile/index" options={{ title: "Profile", href: null }} />
+      <Tabs.Screen name="(drawer)/terms/index" options={{ title: "Terms", href: null }} />
     </Tabs>
-  )
+  );
 }
 
 function getWidth() {
-  let width = Dimensions.get("window").width
+  let width = Dimensions.get("window").width;
 
   // Horizontal Padding = 20...
-  width = width - 80
+  width = width - 80;
 
   // Total five Tabs...
-  return width / 5
+  return width / 5;
 }
